c07820a8f723f01dc52deb7b71bfd057a3207abd
Ashok-BA <ashok972001@outlook.com>
2026-05-27 16:51:20 +0530

    Add project files.

---
+++ CopyConstructor/Program.cs
@@ -0,0 +1,43 @@
+﻿////using System;
+////using System.Collections.Generic;
+////using System.Linq;
+////using System.Text;
+////using System.Threading.Tasks;
+
+////namespace CopyConstructor
+////{
+////    class student
+////    {
+////        public int sid;
+////        public string sname;
+
+////        //public student(int sid, string sname)
+////        //{
+////        //    this.sid = sid;
+////        //    this.sname = sname;
+////        //}
+
+////        public student(student obj)
+////        {
+////            this.sid = obj.sid;
+////            this.sname = obj.sname;
+////        }
+////        public void StudentDisplay()
+////        {
+////            Console.WriteLine("Student ID is:" + sid);
+////            Console.WriteLine("Student Name is" + sname);
+////        }
+////    }
+////    internal class Program
+////    {
+////        static void Main(string[] args)
+////        {
+////            student obj1 = new student(1,"Rama");
+////            obj1.StudentDisplay();
+
+////            student obj2 = new student(obj1);
+////            obj2.StudentDisplay();
+
+////        }
+////    }
+////}
---
+++ CopyConstructor/program2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyConstructor
+{
+    public class person
+    {
+        public int age;
+        public string name;
+
+        public person(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+        public person(person previousperson)
+        {
+            age = previousperson.age;
+            name = previousperson.name;
+        }
+
+        public string Details()
+        {
+            return name + "is" + age.ToString();
+        }
+    }
+
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            person p1 = new person(40, "sam");
+            person p2 = new person(p1);
+            p1.age = 39;
+            p2.age = 42;
+            p2.name = "rama";
+            Console.WriteLine(p1.Details());
+            Console.WriteLine(p2.Details());
+        }
+    }
+}
